14d9459f6567d295f46d9e587076f30280f53ba8
PatriciaMB <patriciamarquesboff@hotmail.com>
2019-06-25 11:34:38 -0300

    Camada de Chess e Imprimindo Tabuleiro

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,17 @@
+package boardgame;
+
+//Posição simples de matriz
+public class Piece {
+	
+	protected Position position;
+	private Board board;
+	
+	public Piece(Board board) {
+		this.board = board;
+		position = null;
+	}
+
+	protected Board getBoard() {
+		return board;
+	}	
+}
